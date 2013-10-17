bf84ba6d1580e07dd31cd115fdbc62ddcf4a8f99
Lee Cusack <leecusack@new-host-3.home>
2013-10-17 11:11:55 -0400

    Initial Commit

---
+++ TalkApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TalkApp
+//
+//  Created by Lee Cusack on 10/17/13.
+//  Copyright (c) 2013 Lee Cusack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TalkApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TalkApp
+//
+//  Created by Lee Cusack on 10/17/13.
+//  Copyright (c) 2013 Lee Cusack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
